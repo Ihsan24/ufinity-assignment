b0e39408aa50616dfe0bc037d1dd9cadd9035c22
M. Ihsan Sudarman <mihsansudarman@ms-macbook-pro.lan>
2023-10-09 22:40:33 +0800

    get reverseGeocode, set geocodeData, setOptions

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,42 +1,88 @@
-import { useEffect, useState } from "react";
-import { Select, Image } from "antd";
+import { useEffect, useRef, useState } from "react";
+import { Select, Image, Space, DatePicker, TimePicker, Button } from "antd";
+import dayjs from "dayjs";
+import { isEmpty, get } from "lodash";
 import styles from "@/styles/Home.module.css";
 
 export default function Home() {
+  const [selectedDate, setSelectedDate] = useState(dayjs().format("YYYY-MM-DD"));
+  const [selectedTime, setSelectedTime] = useState(dayjs().format("HH:mm:ss"));
   const [trafficImgData, setTrafficImgData] = useState([]);
-  const [selectOptions, setSeletOptions] = useState([]);
   const [selectedData, setSelectedData] = useState("");
+  const [locationSelectOptions, setLocationSelectOptions] = useState([]);
+  const [geocodeData, setGeocodeData] = useState([]);
 
-  const onSetSelectOptions = (data) => {
-    const arr = [];
-    data.forEach((item) => {
-      arr.push({
-        value: item.camera_id,
-        label: item.location.latitude + ", " + item.location.longitude,
-        image: item.image,
-      });
-    });
-    setSeletOptions(arr);
+  const resetData = () => {
+    setTrafficImgData([]);
+    setLocationSelectOptions([]);
+    setSelectedData("");
   };
 
   const onSelectLocationHandler = (val) => {
     const obj = trafficImgData.find((x) => x.camera_id === val);
-    console.log("obj: ", obj);
-    if(obj) setSelectedData(obj);
+    console.log('obj: ', obj);
+    if (obj) setSelectedData(obj);
   };
 
-  useEffect(() => {
-    fetch(`https://api.data.gov.sg/v1/transport/traffic-images?date_time=2023-07-30T15:05:00`)
+  const onReverseGeocode = async (items) => {
+    const arr = [...items];
+    const result = [];
+
+    for (let i = 0; i < arr.length; i++) {
+      try {
+        const response = await fetch(
+          `https://geocode.maps.co/reverse?lat=${items[i].location.latitude}&lon=${items[i].location.longitude}`,
+          {
+            method: "GET",
+            headers: {
+              accept: "application/json",
+            },
+          }
+        );
+
+        const data = await response.json();
+        result.push({
+          latlongStr: items[i].location.latitude + "," + items[i].location.longitude,
+          lat: data.lat,
+          lon: data.lon,
+          address: data.address,
+          display_name: data.display_name,
+          place_id: data.place_id,
+        });
+      } catch (error) {
+        // return { lat: item.location.latitude, lon: item.location.longitude, isFetchError: true };
+        // console.log("catch error: ", error);
+      }
+    }
+    setGeocodeData([...geocodeData, ...result]);
+  };
+
+  const onCheckGeocode = (data) => {
+    const result = data.filter(
+      (item) => !geocodeData.some((geoItem) => geoItem.latlongStr === item.location.latitude + "," + item.location.longitude)
+    );
+    onReverseGeocode(result);
+  };
+
+  const onGetTrafficImgHandler = () => {
+    resetData();
+
+    fetch(`https://api.data.gov.sg/v1/transport/traffic-images?date_time=${selectedDate}T${selectedTime}`)
       .then((res) => res.json())
       .then((data) => {
-        console.log("traffic images data: ", data);
-        setTrafficImgData(data.items[0].cameras);
-        onSetSelectOptions(data.items[0].cameras);
+        if (data.items[0]?.cameras) {
+          onCheckGeocode(data.items[0].cameras);
+          setTrafficImgData(data.items[0].cameras);
+        } else {
+          //display no data found
+        }
       })
       .catch((error) => {
         console.log("traffic images data error: ", error);
       });
+  };
 
+  useEffect(() => {
     fetch(`https://api.data.gov.sg/v1/environment/2-hour-weather-forecast?date_time=2023-07-30T15:05:00`)
       .then((res) => res.json())
       .then((data) => {
@@ -47,21 +93,49 @@ export default function Home() {
       });
   }, []);
 
+  useEffect(() => {
+    let options = [];
+    const result = trafficImgData.map((item) => {
+      const data = geocodeData.find((geoItem) => geoItem.latlongStr === item.location.latitude + "," + item.location.longitude);
+      if (data) {
+        options.push({
+          value: item.camera_id,
+          label: data.display_name,
+          image_metadata: item.image_metadata,
+        });
+      }
+      return { ...item, geocode: data };
+    });
+
+    setLocationSelectOptions(options);//for select dropdown options
+    setTrafficImgData(result);//setState again with geocode data
+  }, [geocodeData]);
+
   return (
     <>
       <h1>Ufinity Assignment</h1>
-      <Select
-        showSearch
-        style={{
-          width: 500,
-        }}
-        placeholder="select a location"
-        onChange={onSelectLocationHandler}
-        options={selectOptions}
-        optionFilterProp="children"
-        filterOption={(input, option) => (option?.label ?? "").includes(input)}
-        filterSort={(optionA, optionB) => (optionA?.label ?? "").toLowerCase().localeCompare((optionB?.label ?? "").toLowerCase())}
-      />
+      <Space>
+        <DatePicker onChange={(val) => setSelectedDate(dayjs(val).format("YYYY-MM-DD"))} />
+        <TimePicker onChange={(val) => setSelectedTime(dayjs(val).format("HH:mm:ss"))} />
+        <Button type="primary" onClick={onGetTrafficImgHandler}>
+          Search
+        </Button>
+      </Space>
+      <div>
+        <Select
+          showSearch={true}
+          style={{
+            width: 500,
+          }}
+          placeholder="select a location"
+          onChange={onSelectLocationHandler}
+          options={locationSelectOptions}
+          optionFilterProp="children"
+          filterOption={(input, option) => (option?.label ?? "").includes(input)}
+          filterSort={(optionA, optionB) => (optionA?.label ?? "").toLowerCase().localeCompare((optionB?.label ?? "").toLowerCase())}
+        />
+      </div>
+
       {selectedData && (
         <div>
           <Image width={selectedData.image_metadata?.width < 600 ? selectedData.image_metadata.width : 600} src={selectedData.image} />
